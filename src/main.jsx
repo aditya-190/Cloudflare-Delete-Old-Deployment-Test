a6ac99b5765909f7939a2d379656eb66233c95ef
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2023-04-21 14:47:54 +0530

    Sample Test 3

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <div className="app">Test 2</div>
+    <div className="app">Test 3</div>
 )
